33a244e9c15b3338425f705672f446ddb92c91c7
57480075 <ishidakunpixarsoul@gmail.com>
2021-10-18 00:44:46 +0900

    修正依頼_2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,8 +26,8 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
-    unless user_signed_in?
-      redirect_to :index
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to prototypes_path
     end
   end
 
